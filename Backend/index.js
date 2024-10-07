d026ddf71d25550b716a637f89e182cc707cece2
Akshat Trivedi <akshattrivedi32@gmail.com>
2024-10-07 14:47:32 +0530

    cors policy changes

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://azure-ai-project-frontend.vercel.app/',
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 
 // Register routes
